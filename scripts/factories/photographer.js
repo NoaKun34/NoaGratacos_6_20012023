05bd7936658025c15056141e4bc5070a3b8569e8
Noa <noa.gratacos@orange.fr>
2023-03-08 13:17:36 +0100

    Home page
    Home page finish

--- scripts/factories/photographer.js
+++ scripts/factories/photographer.js
@@ -1,16 +1,41 @@
 function photographerFactory(data) {
-    const { name, portrait } = data;
+    const { name, id, city, country, tagline, price, portrait } = data;
 
-    const picture = `assets/photographers/${portrait}`;
+    const picture = `assets/photos/photographers_selfi/${portrait}`;
+    const location = `${city}, ${country}`;
+    const tjm = `${price}€/jour`;
+
+    console.log("Test dans la fonction photographerFactory");
 
     function getUserCardDOM() {
-        const article = document.createElement( 'article' );
-        const img = document.createElement( 'img' );
+        const article = document.createElement('article');
+        article.setAttribute("id", "photographerContainer");
+        const img = document.createElement('img');
         img.setAttribute("src", picture)
-        const h2 = document.createElement( 'h2' );
+        const link = document.createElement('a');
+        link.href = `../../photographer.html?id=${id}`;
+        link.setAttribute("aria-label", name);
+        const h2 = document.createElement('h2');
         h2.textContent = name;
-        article.appendChild(img);
-        article.appendChild(h2);
+        const locationElement = document.createElement('p');
+        locationElement.setAttribute("id", "location");
+        locationElement.setAttribute("class", "desc");
+        locationElement.textContent = location;
+        const taglineElement = document.createElement('p');
+        taglineElement.setAttribute("id", "tagline");
+        taglineElement.setAttribute("class", "desc");
+        taglineElement.textContent = tagline;
+        const tjmElement = document.createElement('p');
+        tjmElement.setAttribute("id", "tjm");
+        tjmElement.setAttribute("class", "desc");
+        tjmElement.textContent = tjm;
+
+        link.appendChild(img);
+        link.appendChild(h2);
+        article.appendChild(link);
+        article.appendChild(locationElement);
+        article.appendChild(taglineElement);
+        article.appendChild(tjmElement);
         return (article);
     }
     return { name, picture, getUserCardDOM }
