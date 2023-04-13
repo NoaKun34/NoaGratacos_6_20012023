5c1d29932b25100060c73290f8da003483ad1ff9
Noa <noa.gratacos@orange.fr>
2023-04-13 11:50:09 +0200

    Add displayMedia

---
+++ scripts/factories/media.js
@@ -0,0 +1,105 @@
+function selectFactory(data) {
+    const {image, video} = data;
+    const dataContainer = document.querySelector(".dataContainer");
+
+    if (image === undefined ) {
+        const videoModel = videoFactory(data);
+        const videoDOM = videoModel.getMediaCardDOM();
+        dataContainer.appendChild(videoDOM);
+    } else if (video === undefined) {
+        const imageModel = imageFactory(data);
+        const imageDOM = imageModel.getMediaCardDOM();
+        dataContainer.appendChild(imageDOM);
+    }
+
+    return 0;
+}
+
+function imageFactory(data) {
+    const { id, photographerId, title, image, likes, date, price } = data;
+
+    const imagePath = `assets/photos/${photographerId}/${image}`;
+
+    function getMediaCardDOM() {
+        const article = document.createElement('article');
+        article.setAttribute("class", "media");
+        
+        const img = document.createElement('img');
+        img.setAttribute("src", imagePath);
+        img.setAttribute("alt", title);
+
+        const descContainer = document.createElement('div');
+        descContainer.setAttribute("class", "desc");
+        
+        const likesElement = document.createElement('p');
+        likesElement.setAttribute("class", "mediaLikes");
+        likesElement.textContent = likes;
+        
+        const infoElement = document.createElement('div');
+        infoElement.setAttribute("class", "mediaInfo");
+        
+        const titleElement = document.createElement('h2');
+        titleElement.setAttribute("class", "mediaTitle");
+        titleElement.textContent = title;
+        
+        infoElement.appendChild(titleElement);
+        infoElement.appendChild(likesElement);
+
+        article.appendChild(img);
+        article.appendChild(infoElement);
+        
+        return (article);
+    }
+    
+    return { id, photographerId, title, imagePath, likes, date, price, getMediaCardDOM };
+}
+
+function videoFactory(data) {
+    const { id, photographerId, title, video, likes, date, price } = data;
+
+    const videoPath = `assets/photos/${photographerId}/${video}`;
+
+    function getMediaCardDOM() {
+        const article = document.createElement('article');
+        article.setAttribute("class", "media");
+
+        const video = document.createElement('video');
+        video.setAttribute("src", videoPath);
+        video.setAttribute("alt", title);
+
+        const descContainer = document.createElement('div');
+        descContainer.setAttribute("class", "desc");
+        
+        const likesElement = document.createElement('p');
+        likesElement.setAttribute("class", "mediaLikes");
+        likesElement.textContent = likes;
+        
+        const infoElement = document.createElement('div');
+        infoElement.setAttribute("class", "mediaInfo");
+        
+        const titleElement = document.createElement('h2');
+        titleElement.setAttribute("class", "mediaTitle");
+        titleElement.textContent = title;
+        
+        infoElement.appendChild(titleElement);
+        infoElement.appendChild(likesElement);
+        
+        article.appendChild(video);
+        article.appendChild(infoElement);
+        
+        return (article);
+    }
+    
+    return { id, photographerId, title, videoPath, likes, date, price, getMediaCardDOM };
+}
+
+
+//function mediaFactory(data) {
+//    const {image, video} = data;
+//
+//    if (image === undefined ) {
+//        videoFactory(data);
+//    } else if (video === undefined) {
+//        imageFactory(data);
+//    }
+//}
--- scripts/pages/photographer.js
+++ scripts/pages/photographer.js
@@ -1,1 +1,74 @@
-//Mettre le code JavaScript lié à la page photographer.html
+const url = new URL(location.href);
+const getParams = url.searchParams.get("id");
+const photographerID = parseInt(getParams);
+
+async function getPhotographersData() {
+    const dataPath = '../../data/photographers.json';
+
+    try {
+        const response = await fetch(dataPath);
+        const data = await response.json();
+
+        return data;
+    } catch (err) {
+        console.log("Erreur")
+    }
+}
+
+
+async function displayMedia(data, photographerId) {
+    const medias = [];
+    //const dataContainer = document.querySelector(".dataContainer");
+  
+    data.forEach((item) => {
+      if (item.photographerId === photographerId) {
+        medias.push(item);
+        console.log(item.likes);
+      }
+    });
+
+    medias.forEach((media) => {
+        //const mediaCard = selectFactory(media);
+        //dataContainer.appendChild(mediaCard);
+        selectFactory(media);
+    });
+    console.log(medias);
+    return medias;
+}
+
+async function displayPhotographerData(data, id) {
+    const photographerData = data.find((item) => item.id === id);
+    const { name, city, country, tagline, portrait } = photographerData;
+    const location = `${city}, ${country}`;
+    const photographersInfoSection = document.querySelector(".photographerInfo");
+    const photographerSelfi = document.getElementById("photographerSelfi");
+    const picture = `assets/photos/photographers_selfi/${portrait}`;
+
+    const h1 = document.createElement("h1");
+    h1.setAttribute("class", "photographerName");
+    h1.textContent = name;
+    const locationElement = document.createElement("p");
+    locationElement.setAttribute("class", "photographerLocation");
+    locationElement.textContent = location;
+    const taglineElement = document.createElement("p");
+    taglineElement.setAttribute("class", "photographerTagline");
+    taglineElement.textContent = tagline;
+    photographerSelfi.setAttribute("src", picture);
+
+    photographersInfoSection.appendChild(h1);
+    photographersInfoSection.appendChild(locationElement);
+    photographersInfoSection.appendChild(taglineElement);
+
+    return photographerData;
+}
+
+//async function displayData(photographerData) {}
+
+async function initPhotographer() {
+    console.log("Test starting photographer");
+    const photographerData = await getPhotographersData();
+    await displayPhotographerData(photographerData.photographers, photographerID);
+    await displayMedia(photographerData.media, photographerID);
+}
+
+initPhotographer();
